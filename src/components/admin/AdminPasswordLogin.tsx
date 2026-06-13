77e1a5ff48ec1c9b80a2d64794bb6411bef09284
Abderrahil Cheikh <cheikh.abderrahim@gmail.com>
2026-06-13 22:59:26 +0100

    style: force flat orange admin login button

--- src/components/admin/AdminPasswordLogin.tsx
+++ src/components/admin/AdminPasswordLogin.tsx
@@ -129,6 +129,7 @@ export default function AdminPasswordLogin({ lang }: Props) {
 
           <button
             type="submit"
+            className="vemo-admin-login-button"
             style={{
               marginTop: 22,
               width: "100%",
